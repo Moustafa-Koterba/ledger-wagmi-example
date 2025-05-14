98e5f49e857082581910095122868961be267e17
Moustafa KOTERBA <moustafa.koterba@ledger.fr>
2025-05-14 11:00:45 +0200

    init project

---
+++ src/wagmi.ts
@@ -0,0 +1,24 @@
+import { http, createConfig } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+import { custom } from "viem";
+import { LedgerLiveEthereumProvider } from "./provider";
+
+export const config = createConfig({
+  chains: [mainnet, sepolia],
+  connectors: [
+    injected(),
+    coinbaseWallet(),
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+  ],
+  transports: {
+    [mainnet.id]: custom(new LedgerLiveEthereumProvider()),
+    [sepolia.id]: http(),
+  },
+});
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
